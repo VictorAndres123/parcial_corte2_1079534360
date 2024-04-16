7fb224e9c2a66f570195290ee9b9d23af73ce950
VictorAndres123 <132307617+VictorAndres123@users.noreply.github.com>
2024-04-16 07:10:19 -0500

    Add files via upload

---
+++ Frontend/shopping/src/components/CategoryList.vue
@@ -0,0 +1,34 @@
+<template>
+    <ion-page>
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Lista de Categorías</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <ion-list>
+          <ion-item v-for="category in categories" :key="category.id">
+            <ion-label>{{ category.name }}</ion-label>
+            <ion-label slot="end">{{ category.description }}</ion-label>
+          </ion-item>
+        </ion-list>
+      </ion-content>
+    </ion-page>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        categories: [] // Aquí deberías tener una lista de categorías
+      };
+    },
+    mounted() {
+      // Lógica para cargar las categorías desde la API
+      // axios.get('URL_DE_TU_API/categorias').then(response => {
+      //   this.categories = response.data;
+      // });
+    }
+  };
+  </script>
+  
---
+++ Frontend/shopping/src/components/ProductList.vue
@@ -0,0 +1,35 @@
+<template>
+    <ion-page>
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Lista de Productos</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <ion-list>
+          <ion-item v-for="product in products" :key="product.id">
+            <ion-label>{{ product.name }}</ion-label>
+            <ion-label slot="end">{{ product.price }}</ion-label>
+          </ion-item>
+        </ion-list>
+      </ion-content>
+    </ion-page>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        products: [] // Aquí deberías tener una lista de productos
+      };
+    },
+    mounted() {
+      // Lógica para cargar los productos desde la API
+      // Por ejemplo, usando Axios para hacer una solicitud HTTP
+      // axios.get('URL_DE_TU_API/productos').then(response => {
+      //   this.products = response.data;
+      // });
+    }
+  };
+  </script>
+  
